ed3f0ce7c33b31d0e3a58d27197411ab0c54df3f
Ian <ianthomasevistoN@gmail.com>
2021-02-18 17:21:57 -0500

    Refactored, reorganized, and made more extensible

--- src/commandbrd.ts
+++ src/commandbrd.ts
@@ -1,24 +1,25 @@
-interface ConcreteCommandbrdClass<T extends Commandbrd> { new(): T };
+interface ConcreteCommandbrdClass extends Commandbrd { new(...args: any): Commandbrd };
 
 export default abstract class Commandbrd {
     public abstract readonly names: string[];
     public abstract readonly info: string;
     public abstract readonly usage: string;
 
-    protected readonly subCommands: ConcreteCommandbrdClass<Commandbrd>[] = [];
-    
+    public abstract run(): Promise<void>;
+
+    protected readonly subCommands: ConcreteCommandbrdClass[] = [];
     public subCommandInstances: Commandbrd[] = [];
 
     public init(): void {
-        this.subCommandInstances = this.subCommands.map(commandClass => Commandbrd.newCommand(commandClass));
+        this.subCommandInstances = this.subCommands.map(commandClass => {
+            const newCommand = this.newCommand(commandClass);
+            newCommand.init();
+
+            return newCommand;
+        });
     }
 
-    public static newCommand<T extends Commandbrd>(commandClass: ConcreteCommandbrdClass<T>): T {
-        const commandbrd = new commandClass();
-        commandbrd.init();
-
-        return commandbrd;
+    protected newCommand(commandClass: ConcreteCommandbrdClass): Commandbrd {
+        return new commandClass();
     }
-
-    public abstract run(): Promise<void>;
 }
